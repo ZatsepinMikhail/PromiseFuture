beac3cea73f5282f57bd729f0bec35406f1cd737
ZatsepinMikhail <salamence1996@gmail.com>
2017-04-23 18:09:02 +0300

    add first version of task pool

--- Async.h
+++ Async.h
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <thread>
+#include <vector>
 
 int foo( bool b, int nIterations )
 {
@@ -31,6 +32,10 @@ int fooN()
     return sum;
 }
 
+class TaskPool {
+
+};
+
 template <typename R>
 class MyTask
 {
@@ -41,18 +46,50 @@ public:
 
     void operator()()
     {
-        promiseForResult.SetValue( functionToExecute() );
+        try {
+            promiseForResult.SetValue( functionToExecute() );
+        } catch( std::exception& e ) {
+            promiseForResult.SetException( e );
+        }
     }
 
     MyFuture<R> get_future() { return promiseForResult.GetFuture(); }
 };
 
-template<typename R>
-MyFuture<R> MyAsync( std::function<R()> functionToExecute )
-{
-    MyTask<R> newTask( functionToExecute );
-    MyFuture<R> taskFuture = newTask.get_future();
-    std::thread t1( std::move( newTask ) );
-    t1.detach();
-    return taskFuture;
-}
+class MyAsyncExecutor {
+public:
+    MyAsyncExecutor( unsigned int threadNumber )
+            : workerNumber( std::max( 1U, std::min( threadNumber, std::thread::hardware_concurrency() ) ) ) {}
+
+    template<typename R> MyFuture<R> Execute(std::function<R()> functionToExecute)
+    {
+        MyTask<R> newTask( functionToExecute );
+        MyFuture<R> taskFuture = newTask.get_future();
+
+        // инициализация
+        if( workers.size() < workerNumber ) {
+            // асинхронно
+            std::cout << "async new\n";
+            workers.emplace_back( std::move( newTask ) );
+            return taskFuture;
+        }
+
+        // асинхронно
+        for( auto it = workers.begin(); it != workers.end(); it++ ) {
+            if( it->joinable() ) {
+                it->join();
+                workers.emplace( it, std::move( newTask ) );
+                std::cout << "async\n";
+                return taskFuture;
+            }
+        }
+
+        // синхронно
+        std::cout << "sync\n";
+        newTask();
+        return taskFuture;
+    }
+private:
+    const int workerNumber;
+    std::vector<std::thread> workers;
+};
--- main.cpp
+++ main.cpp
@@ -27,13 +27,17 @@ int main() {
     int nIterations = 1000000000;
 //    auto fut1 = std::async(std::launch::async, foo, true, nIterations);
 //    auto fut2 = std::async(std::launch::async, foo, true, nIterations);
-    auto fut1 = MyAsync<int>( std::bind( foo, true, nIterations) );
-    auto fut2 = MyAsync<int>( fooN );
-    auto fut3 = MyAsync<int>( fooN );
+    MyAsyncExecutor executor( 20 );
+    auto fut1 = executor.Execute<int>( std::bind( foo, true, nIterations) );
+    auto fut2 = executor.Execute<int>( fooN );
+    auto fut3 = executor.Execute<int>( fooN );
+    auto fut4 = executor.Execute<int>( fooN );
+    auto fut5 = executor.Execute<int>( fooN );
+    auto fut6 = executor.Execute<int>( fooN );
     int sum = 0;
     for( int i = 0; i < nIterations; ++i ){
         sum++;
     }
-    std::cout << fut1.GetValue() << ' ' << fut2.GetValue() << " " << fut3.GetValue() << " " << sum << std::endl;
+    std::cout << fut1.GetValue() << " " << fut2.GetValue() << " " << fut3.GetValue() << " " << sum << std::endl;
     return 0;
 }
